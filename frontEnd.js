aff921dcde1e91d4c5668f1fe0abca028a2578ee
Gary Pinsky <kd8tbs@gmail.com>
2019-06-18 16:05:37 -0400

    Images and their containers are added through frontEnd.js Added image and imageContainer class. Added image and imageContainer css style in style.css

--- frontEnd.js
+++ frontEnd.js
@@ -1,8 +1,20 @@
-const testFolder = "./temp";
-const fs = require("fs");
+var imageNames = [
+  "freeX.jpeg",
+  "questionMark.png",
+  "revenge.jpg",
+  "seventeen.jpg",
+  "tallPic.jpg",
+  "widePic.jpg"
+];
+for (i = 0; i < imageNames.length; i++) {
+  var div = document.createElement("div");
+  div.id = i;
+  div.className = "imageContainer";
+  div.innerHTML = '<img src="./temp/' + imageNames[i] + '" class = "image"/>';
+  document.body.appendChild(div);
+}
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
+/*document.getElementById("image1").innerHTML =
+  '<img src="./temp/seventeen.jpg" alt="Free X"/>';
+var div = document.createElement("div2");
+document.body.appendChild(div);*/
